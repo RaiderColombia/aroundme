b50e48fa1ddfc7fcded02483abd19015ba8f06b4
Jhon Ramirez <raidercolombia@gmail.com>
2017-11-15 05:41:10 -0500

    first commit

---
+++ app/src/main/java/aroundme/co/edu/unal/aroundme/MainActivity.java
@@ -0,0 +1,13 @@
+package aroundme.co.edu.unal.aroundme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
